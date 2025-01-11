80c218fe1f4970c7e1a7c90cd6bf5612619858fe
noName <132767802+noNameEXP@users.noreply.github.com>
2025-01-11 18:05:52 +0100

    Update minifier.js

--- minifier.js
+++ minifier.js
@@ -7,22 +7,21 @@ function minifyLua() {
         .replace(/\/\*[\s\S]*?\*\//g, '');
 
     // Replace variables with BanonaVariableNumber
-    const variableRegex = /(\b[a-zA-Z_][a-zA-Z0-9_]*\b)/g;
+    const variableRegex = /\blocal\s+([a-zA-Z_][a-zA-Z0-9_]*)\b/g;
     let variableCounter = 1;
-    const variables = new Set();
+    const variables = new Map();
 
     // Collect all unique variable names
-    minified.replace(variableRegex, (match) => {
-        if (!keywords.has(match)) {
-            variables.add(match);
-        }
+    minified = minified.replace(variableRegex, (match, p1) => {
+        const newVariableName = `BanonaVariable${variableCounter++}`;
+        variables.set(p1, newVariableName);
+        return `local ${newVariableName}`;
     });
 
-    // Replace each variable with BanonaVariableNumber
-    variables.forEach(variable => {
-        const newVariableName = `Banona${variableCounter++}`;
-        const variableReplaceRegex = new RegExp(`\\b${variable}\\b`, 'g');
-        minified = minified.replace(variableReplaceRegex, newVariableName);
+    // Replace each variable usage with BanonaVariableNumber
+    variables.forEach((newVariable, originalVariable) => {
+        const variableReplaceRegex = new RegExp(`\\b${originalVariable}\\b`, 'g');
+        minified = minified.replace(variableReplaceRegex, newVariable);
     });
 
     document.getElementById("luaInput").value = minified;
